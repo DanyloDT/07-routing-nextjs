012f8d85cdb5ceacc9449f38471bc2f58bcc67e4
Danylo Diachenko <danylodiachenko@gmail.com>
2026-03-22 18:26:38 +0200

    update

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,5 +1,10 @@
-import NotePreview from '@/components/NotePreview/NotePreview';
+import NotePreview from './NotePreview.client';
 import { fetchNoteById } from '@/lib/api';
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
 
 type Props = {
   params: Promise<{ id: string }>;
@@ -7,13 +12,14 @@ type Props = {
 
 const NoteModalPreview = async ({ params }: Props) => {
   const { id } = await params;
+  const queryClient = new QueryClient();
 
   const note = await fetchNoteById(id);
 
   return (
-    <>
+    <HydrationBoundary state={dehydrate(queryClient)}>
       <NotePreview note={note} />
-    </>
+    </HydrationBoundary>
   );
 };
 
